4861da70ee0b19d902e7705beb0986997d64e563
Espar-RodBar <cr4wl3r@gmail.com>
2023-07-21 18:13:38 +0200

    added key to response

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const http = require("http");
 const fs = require("fs");
 const dotenv = require("dotenv");
 
+const key = process.env.API_KEY;
+
 const app = http.createServer((request, response) => {
     const url = request.url;
 
@@ -10,7 +12,7 @@ const app = http.createServer((request, response) => {
         fs.readFile(__dirname + "/index.html", "utf-8", (error, data) => {
             if (error) throw new Error("no file found");
             console.log("reading home root");
-            response.end(data);
+            response.end("Sending key..... " + key);
         });
     }
 });
