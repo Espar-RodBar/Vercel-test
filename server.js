131ed150d1b23570aab047d223fcb6b5e134457d
Espar-RodBar <cr4wl3r@gmail.com>
2023-07-22 09:33:36 +0200

    installed express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const url = require("url");
+const express = require("express");
 const http = require("http");
 const fs = require("fs");
 require("dotenv").config();
@@ -7,19 +8,19 @@ const PORT = 8000;
 
 const key = process.env.API_KEY || "no key";
 
-const app = http.createServer((request, response) => {
-    const url = request.url;
+const app = express();
 
-    if (url === "/") {
-        fs.readFile(__dirname + "/index.html", "utf-8", (error, data) => {
-            if (error) throw new Error("no file found");
-            console.log("reading home root");
-            console.log(typeof data, key);
-            const page = data.replace("<% txt %>", "key: " + key);
-            console.log(page);
-            response.end(page);
-        });
-    }
+//const app = http.createServer((request, response) => {}
+//const url = request.url;
+app.get("/", (request, response) => {
+    fs.readFile(__dirname + "/index.html", "utf-8", (error, data) => {
+        if (error) throw new Error("no file found");
+        console.log("reading home root");
+        console.log(typeof data, key);
+        const page = data.replace("<% txt %>", "key: " + key);
+        console.log(page);
+        response.end(page);
+    });
 });
 
 app.listen(process.env.PORT || PORT, "localhost", () => {
